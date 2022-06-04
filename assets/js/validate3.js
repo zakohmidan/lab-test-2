447bbbbd0a74fdcc6fd91305a7c95d646af4fa22
zakohmidan <90730764+zakohmidan@users.noreply.github.com>
2022-06-04 10:46:10 +0000

    Update validate3.js

--- assets/js/validate3.js
+++ assets/js/validate3.js
@@ -112,8 +112,8 @@
             .then(response => response.blob())
             .then(blob => {
                 var url = window.URL.createObjectURL(blob);
-                /*window.open(url);*/
-                window.location.assign(url);
+                window.open(url,'_blank');
+                window.location.assign(url,'_blank');
                 var a = document.createElement('a');
                 a.href = url;
                 a.download = file_name+".pdf";
